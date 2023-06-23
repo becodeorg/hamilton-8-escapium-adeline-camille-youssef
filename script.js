e5a7a01d6a259b61b75a7cec31b80d801d9a121a
youssef <youssefboulaich33@gmail.com>
2023-06-23 12:23:45 +0200

    test

--- script.js
+++ script.js
@@ -9,13 +9,24 @@ function myFunction() {
 
 function myFunctiona() {
   document.getElementById("header-gallery").style.backgroundImage =
-    "url(/images/gallery/gallery-2.jpg)";
+    "url(/images/gallery/slide-1.jpg)";
+  document.querySelector(".headerleft___p").innerHTML = "You<br> are locked in";
+  document.querySelector(".headerleft").style.animation =
+    "example 2s linear";
 }
 function myFunctionb() {
   document.getElementById("header-gallery").style.backgroundImage =
     "url(/images/gallery/slide-4.jpg)";
+  document.querySelector(".headerleft___p").innerHTML =
+    "Escape <br>in 60 mins?";
+  document.querySelector(".headerleft").style.animation =
+    "example 2s linear";
 }
 function myFunctionc() {
   document.getElementById("header-gallery").style.backgroundImage =
-    "url(/images/gallery/gallery-4.jpg)";
+    "url(/images/gallery/slide-2.jpg)";
+  document.querySelector(".headerleft___p").innerHTML =
+    "The Clock <br>is Ticking";
+  document.querySelector(".headerleft").style.animation =
+    "example 2s linear";
 }
